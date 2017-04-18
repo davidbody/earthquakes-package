b69430dea10aae7ef57fa27dea0d0c63ffad7d53
David W. Body <davidbody@bigcreek.com>
2017-04-18 17:25:44 -0500

    Convert EQ_PRIMARY and TOTAL_DEATHS to numeric

--- R/clean_data.R
+++ R/clean_data.R
@@ -14,6 +14,7 @@ eq_location_clean <- function(location) {
 #'
 #' A DATE column is created based on YEAR, MONTH, DAY.
 #' LATITUDE and LONGITUDE are converted to numeric.
+#' EQ_PRIMARY and TOTAL_DEATHS are converted to numeric.
 #' LOCATION_NAME is cleaned by passing it to the \code{\link{eq_location_clean}} function.
 #'
 #' @param earthquakes A data frame containing the earthquakes data set.
@@ -21,9 +22,13 @@ eq_location_clean <- function(location) {
 #' @return A new data frame with cleaned earthquakes data.
 eq_clean_data <- function(earthquakes) {
     result <- data.frame(earthquakes)
-    result$DATE <- as.Date(mapply(to_date, result$YEAR, result$MONTH, result$DAY), origin = as.Date("1970-01-01"))
+    result$DATE <- as.Date(mapply(to_date, result$YEAR,
+                                  result$MONTH, result$DAY),
+                           origin = as.Date("1970-01-01"))
     result$LATITUDE <- as.numeric(result$LATITUDE)
     result$LONGITUDE <- as.numeric(result$LONGITUDE)
+    result$EQ_PRIMARY <- as.numeric(result$EQ_PRIMARY)
+    result$TOTAL_DEATHS <- as.numeric(result$TOTAL_DEATHS)
     result$LOCATION_NAME <- eq_location_clean(result$LOCATION)
     result
 }
